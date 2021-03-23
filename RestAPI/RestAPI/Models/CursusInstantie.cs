08dfab64548be38cd99b17f66f42ded319dcdd8e
PugMyLife <74055007+PugMyLife@users.noreply.github.com>
2021-03-23 09:15:03 +0100

    CursusInstantieModel Added

---
+++ RestAPI/RestAPI/Models/CursusInstantie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class CursusInstantie
+    {
+        public DateTime startDatum { get; set; }
+        public CursusDetail cursusDetail { get; set; }
+    }
+}
---
+++ RestAPI/UnitTests/UnitTestsCursusInstantie.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestsCursusInstantie
+    {
+        [TestMethod]
+        public void NewCursusInstantieIsValid()
+        {
+            var cursusInput = new CursusInstantie
+            {
+                startDatum = new DateTime(2021, 12, 03),
+                cursusDetail = new CursusDetail {
+                    Duur = 3,
+                    Titel = "eindcase",
+                    Code = "EICE" }
+            };
+
+            var validationResults = new List<ValidationResult>();
+            var actual = Validator.TryValidateObject(cursusInput, new ValidationContext(cursusInput), validationResults, true);
+
+            Assert.IsTrue(actual, "Expected validation to succeed.");
+
+        }
+    }
+}
